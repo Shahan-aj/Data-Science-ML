47515443b5dd7edeee932e710a7344abb8eada2f
Shahan-aj <134576378+Shahan-aj@users.noreply.github.com>
2023-10-08 01:36:59 +0200

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Data Science Tools and Ecosystem
+
+# ### Introduction
+# 
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# #### Objectives:
+#  
+#  - Listing popular languages for Data Science
+#  - Listing some libraries for Data Science
+#  - some of the data science tool
+#  - evaluating some arithmetic expressions 
+#   
+
+# #### Some of the popular languages that Data Scientists use are:
+#     
+#     1. Python
+#     2. R
+#     3. Scala
+#     4. Java
+#     5. Julia
+
+# #### Some of the commonly used libraries used by Data Scientists include:
+#     
+#     1. NumPy
+#     2. Keras
+#     3. Pandas
+#     4. PyTorch
+#     5. SciPy
+
+# #### Data Science Tools
+# 
+# | Tool Name                | Description                                   | Website                                          |
+# |--------------------------|-----------------------------------------------|--------------------------------------------------|
+# | Python                   | A versatile programming language for data analysis and machine learning. | [python.org](https://www.python.org/)            
+# | Jupyter Notebook         | An interactive web-based notebook for data exploration and analysis. | [jupyter.org](https://jupyter.org/)              
+# | R                        | A programming language and environment for statistical computing and graphics. | [r-project.org](https://www.r-project.org/)     
+# | Pandas                   | A Python library for data manipulation and analysis. | [pandas.pydata.org](https://pandas.pydata.org/)  
+# | NumPy                    | A Python library for numerical computations and array operations. | [numpy.org](https://numpy.org/)                 
+# 
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+#  
+#  #### Addition
+#    5+3 = 8
+#  
+#  #### Substraction
+#    10-3 = 7
+#    
+#  #### Multiplication
+#    2*3 = 6
+#  
+#  #### Division
+#    9/3 = 3
+# 
+
+# In[5]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+
+a = 5
+b = 3
+c = 2
+print((a*b)+c)
+
+
+# In[7]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+
+minutes = 200
+
+hours = minutes/60
+print(hours)
+
+
+# ## Author
+# 
+# Akhmal Shahan
+
+# In[ ]:
+
+
+
+
